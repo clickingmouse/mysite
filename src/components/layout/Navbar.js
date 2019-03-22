0e63515d7cec711755fb23117dfef297d445903b
Kevin K <clickingmouse@hotmail.com>
2019-03-22 20:57:55 +0800

    added sidenav

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,13 +3,51 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="nav-wrapper grey darken-3">
-      <div className="container">
-        <NavLink to="/" className="brand-logo">
-          My Site
-        </NavLink>
-      </div>
-    </nav>
+    <div>
+      <nav className="nav-wrapper light-blue lighten-4">
+        <div className="container">
+          <NavLink to="/" className="brand-logo">
+            My Site
+          </NavLink>
+          <NavLink
+            to="#"
+            className="sidenav-trigger"
+            data-target="mobile-links"
+          >
+            <i className="material-icons">menu</i>
+          </NavLink>
+          <ul className="right hide-on-med-and-down">
+            <li>
+              <NavLink to="#">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="#">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="#">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="#">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="#">Home</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul className="sidenav" id="mobile-links">
+        <li>
+          <NavLink to="#">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="#">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="#">Contact</NavLink>
+        </li>
+      </ul>
+    </div>
   );
 };
 
