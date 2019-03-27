04e04eb8b20275256aed665606cfc98bf0fd3724
Kevin K <clickingmouse@hotmail.com>
2019-03-27 16:40:55 +0800

    simple basic parallax

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import backgroundImg from "../../images/layout/hong-kong-ariel.jpg";
 //import image from "../../images/layout/";
+import quarrybayImg from "../../images/layout/quarry-bay.jpg";
 import "./header.css";
 
 export default function Header() {
@@ -11,6 +12,23 @@ export default function Header() {
           backgroundImage: "url(" + backgroundImg + ")"
         }}
       />
+      <div className="container">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
+        possimus magni eveniet quae porro voluptatibus, in quibusdam dicta
+        minima, nisi laudantium autem odio perferendis architecto est similique
+        unde eius accusantium?
+      </div>
+      <div className="parallax-container">
+        <div className="parallax">
+          <img src={quarrybayImg} alt="" className="responsive-img" />
+        </div>
+      </div>
+      <div className="container">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
+        possimus magni eveniet quae porro voluptatibus, in quibusdam dicta
+        minima, nisi laudantium autem odio perferendis architecto est similique
+        unde eius accusantium?
+      </div>
     </div>
   );
 }
