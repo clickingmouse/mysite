9252af171e6697cbc0541583989134221ea40562
Kevin K <clickingmouse@hotmail.com>
2019-03-28 10:30:18 +0800

    tweaked parallax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
-import Header from "./components/layout/Header";
+import Landing from "./components/layout/Landing";
+//import Header from "./components/layout/Header";
 import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
@@ -9,7 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Header />
+          <Landing />
         </div>
       </BrowserRouter>
     );
--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,17 +1,32 @@
 import React from "react";
 import backgroundImg from "../../images/layout/hong-kong-ariel.jpg";
 //import image from "../../images/layout/";
+import landingImg from "../../images/layout/hong-kong-ariel-1920.jpg";
 import quarrybayImg from "../../images/layout/quarry-bay.jpg";
 import "./header.css";
 
-export default function Header() {
+export default function Landing() {
   return (
     <div>
-      <header
-        style={{
-          backgroundImage: "url(" + backgroundImg + ")"
-        }}
-      />
+      <div id="test" className="parallax-container center valign-wrapper test1">
+        <div className="container">
+          <div className="row">
+            <div className="col s12 white-text">
+              <h2 className="teal-text teal-text lighten-2">
+                Parallax Template
+              </h2>
+              <p>
+                A modern responsive front-end framework based on Material Design
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="parallax">
+          <img src={landingImg} alt="" className=" landingImg" />
+        </div>
+      </div>
+
       <div className="container">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
         possimus magni eveniet quae porro voluptatibus, in quibusdam dicta
@@ -26,7 +41,7 @@ export default function Header() {
               <p>
                 A modern responsive front-end framework based on Material Design
               </p>
-              <a class="waves-effect waves-light btn-large teal lighten-2">
+              <a className="waves-effect waves-light btn-large teal lighten-2">
                 Get Started
               </a>
             </div>
