a39571c88ec1a489a5916741f708db21fc3cafe6
clickingmouse <clickingmouse@users.noreply.github.com>
2019-06-04 09:13:26 +0800

    changed header color to deep-orange

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <nav className="nav-wrapper light-blue lighten-4">
+      <nav className="nav-wrapper deep-orange">
         <div className="container">
           <NavLink to="/" className="brand-logo">
             My Site
