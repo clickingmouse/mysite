0d13ffb0e7b35e79f8719c40090415fdf00e80da
clickingmouse <clickingmouse@users.noreply.github.com>
2019-06-07 20:09:59 +0800

    adding yahoo weather api

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,6 +1,58 @@
+const axios = require("axios");
+const OAuth = require("oauth");
+
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello World"
-  });
+  const YAHOO_API_URL = process.env.REACT_APP_YAHOO_API_URL;
+  const YAHOO_API_CLIENT_ID = process.env.REACT_APP_YAHOO_API_CLIENT_ID;
+  const YAHOO_API_CLIENT_SECRET = process.env.REACT_APP_YAHOO_API_CLIENT_SECRET;
+  const YAHOO_API_ID = process.env.REACT_APP_YAHOO_API_ID;
+
+  //send user response
+  const send = body => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body)
+    });
+  };
+
+  // perform api call
+  const getUsers = () => {
+    console.log("---===>>>");
+    //console.log(YAHOO_API_KEY);
+    console.log(YAHOO_API_ID);
+    console.log(YAHOO_API_CLIENT_ID);
+    console.log(YAHOO_API_CLIENT_SECRET);
+
+    var header = {
+      "X-Yahoo-App-Id": YAHOO_API_ID
+    };
+    var request = new OAuth.OAuth(
+      null,
+      null,
+      YAHOO_API_CLIENT_ID,
+      YAHOO_API_CLIENT_SECRET,
+      "1.0",
+      null,
+      "HMAC-SHA1",
+      null,
+      header
+    );
+    request.get(
+      "https://weather-ydn-yql.media.yahoo.com/forecastrss?location=sunnyvale,ca&format=json",
+      null,
+      null,
+      function(err, data, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+          send(data);
+        }
+      }
+    );
+  };
+  // make sure method is GET
+  if (event.httpMethod == "GET") {
+    getUsers();
+  }
 };
